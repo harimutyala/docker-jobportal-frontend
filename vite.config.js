7dae2c4ae0198fa6d8dca0237d41a1da7d8fe635
harimutyala <2300090101@kluniversity.in>
2025-09-23 09:31:33 +0530

    commited

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // default, where npm run build will output
+  },
+  base: '/frontapp1/', // 👈 important: matches your Tomcat context path
 })
